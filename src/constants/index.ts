bd765fcbc0a3c167f111df81af11630fdfee2282
hank <hank@show.one>
2020-11-10 23:56:02 +0800

    debug

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x8de7be9FBAF5d8A8eE1680D8Ba82669b7c0C5daf'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
