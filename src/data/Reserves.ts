3fc6a7166a75cae6681e95770f55b6f9116d8200
sunker0115 <sunker0115@163.com>
2020-11-11 20:03:27 +0800

    fix beefswap/sdk

--- src/data/Reserves.ts
+++ src/data/Reserves.ts
@@ -1,6 +1,6 @@
-import { TokenAmount, Pair, Currency } from '@uniswap/sdk'
+import { TokenAmount, Pair, Currency } from '@beefswap/sdk'
 import { useMemo } from 'react'
-import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
+import { abi as IUniswapV2PairABI } from '@beefswap/core/build/IBeefswapV2Pair.json'
 import { Interface } from '@ethersproject/abi'
 import { useActiveWeb3React } from '../hooks'
 
--- src/state/transactions/actions.ts
+++ src/state/transactions/actions.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit'
-import { ChainId } from '@uniswap/sdk'
+import { ChainId } from '@beefswap/sdk'
 
 export interface SerializableTransactionReceipt {
   to: string
